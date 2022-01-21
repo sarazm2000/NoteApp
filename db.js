f5add67f7a694f980bc7c33a4e04e5fc1ddf4124
Dariush <59167222+dariusamiri@users.noreply.github.com>
2022-01-22 00:45:59 +0330

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "postgres",
@@ -8,14 +7,3 @@ const pool = new Pool({
     database: "noteapp",
 });
 module.exports = pool;
-=======
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: "postgres",
-    password: "o91759o9576",
-    host: "localhost",
-    port: "5432",
-    database: "noteapp",
-});
-module.exports = pool;
->>>>>>> 29a8a1a6826dfb5b5ef1e483573e1740bca2dd5e
